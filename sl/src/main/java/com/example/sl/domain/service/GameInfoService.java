3d4e22470ba67c613127b9b167c77e5da785147c
jong12mm <jong12mm@naver.com>
2024-06-24 15:33:50 +0900

    .

---
+++ sl/src/main/java/com/example/sl/domain/service/GameInfoService.java
@@ -0,0 +1,9 @@
+package com.example.sl.domain.service;
+
+import com.example.sl.domain.dto.GameInfoDto;
+
+import java.util.List;
+
+public interface GameInfoService {
+    List<GameInfoDto> getAllGameInfo();
+}
