696288f86abcfe8378659d0454dd8a29b354072e
이종일 <jong12mm@naver.com>
2024-07-01 01:15:35 +0900

    .

--- sl/src/main/java/com/example/sl/repository/GameInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sl.repository;
-
-import com.example.sl.entity.GameInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameInfoRepository extends JpaRepository<GameInfoEntity, Long> {
-}
